183b8b24300039bdf6e931be520a35905a1eeb01
prabhjotkb <41100291+prabhjotkb@users.noreply.github.com>
2018-07-27 14:40:19 +0530

    Update delete.php

--- bakery/delete.php
+++ bakery/delete.php
@@ -11,13 +11,5 @@ echo "<script>";
         echo "window.alert('Data Deleted')
 	window.location.href='view.php';
 </script>";
-/*
-//if($getResult===true ){
-if($conn->query($que)=== true){
-
-	
-	//echo "<script>window.open('view.php?delete=Record Has been Deleted Successfully!','_self')<script>";
-}
-//}*/
 
 ?>
