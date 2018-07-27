3b70b9664c6fa34076634c6f1911933fa8340d73
prabhjotkb <41100291+prabhjotkb@users.noreply.github.com>
2018-07-27 22:59:18 +0530

    Update index.php

--- bakery/index.php
+++ bakery/index.php
@@ -43,7 +43,7 @@ $getResults= sqlsrv_query($conn, $tsql);
 
 //if($conn->query($que)=== true)
 //{
-	echo "<center><b>The follwing Data has been inserted into our databse:</b></center>";
+	echo "<font colour='white'><center><b>The follwing Data has been inserted into our databse:</b></center>";
 
 //}
 }
