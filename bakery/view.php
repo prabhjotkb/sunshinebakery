cf1e2f17a4c725ec8ba858781ee96fed1f4e7969
prabhjotkb <41100291+prabhjotkb@users.noreply.github.com>
2018-07-27 16:16:18 +0530

    Update view.php

--- bakery/view.php
+++ bakery/view.php
@@ -53,7 +53,7 @@
 				<td><?php echo $productordered; ?></td>
 				<td><?php echo $productquantity; ?></td>
 				<td><a href ='delete.php?del=<?php echo $phonenumber; ?>'>Delete</a></td>
-				<td><a href='edit.php?edit=<?php echo $phonenumber ?>'>Edit</a></td>
+				<td><a href='edit.php?edit=<?php echo $phonenumber; ?>'>Edit</a></td>
 				
 					</tr>
 			<?php } ?>
