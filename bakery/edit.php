f37a2cbecec535f04a39dbd7395c3d09ea22eba0
prabhjotkb <41100291+prabhjotkb@users.noreply.github.com>
2018-07-28 06:13:17 +0000

    Update edit.php

--- bakery/edit.php
+++ bakery/edit.php
@@ -5,5 +5,16 @@
 
 	//$query="SELECT * FROM dbo.sunbakery1 WHERE id='$edit_record'";
 	$getResults=sqlsrv_query($conn, $query);
-       echo $getResults;
-	?>
+       //echo $getResults;
+	while($row=sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+	{
+   	 $id = $_POST['id'];
+   	 $customername=$_POST['customername'];
+	$phonenumber=$_POST['phonenumber'];
+	$pickupdate=$_POST['pickupdate'];	
+   	$productordered=$_POST['productordered'];
+   	$productquantity=$_POST['productquantity'];
+	}
+echo $customername;
+?>
+
