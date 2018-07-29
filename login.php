ecc6778307c990fe43d024a0b6a2d2696f4291e0
prabhjotkb <41100291+prabhjotkb@users.noreply.github.com>
2018-07-29 10:53:57 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,41 +1,87 @@
 <?php
+include ('config.php');
 session_start();
+if(isset($_POST['login_btn'])){
+    
+$username_var=$_POST['username_box'];
+$password_var=$_POST['password_box'];    
+$_SESSION['username']=$username;
+  //  echo "<script>alert($username_var)</script>" ;
+    
+    //  echo "<script>alert($password_var)</script>" ;
+    if($username_var=="admin"&&$password_var=="admin"){
+        
+        header('Location:view.php');
+    }else{
+        
+           
+  $queryS="select * from dbo.login where username='$username_var' and password_='$password_var'";  
+$result=sqlsrv_query($conn,$queryS);
+$row=sqlsrv_fetch_array($result);
+    
+         if($username_var==null || $username_var=="" && $password_var==null || $password_var=="")
+         {
+    echo "<script>alert('Please enter a valid Username or Password!')</script>";
+            }
+       
+    
+    else if($row['username']==$username_var && $row['password_']==$password_var)
+    {
+   
+        
+       // echo "<script>alert('Login Succesfull!');</script>";
+        header('Location:bakery/view.php');
+}
+else{
+   // echo "<script>alert('Login Failed!');</script>";
+    header('Location:index.html');
+}
+    }
+}
+?>
 ?>
 <html>
     <head>
-        <title>sunshine bakery</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="assets/css/style.css" />
+    <title>Sign In</title>
+        <style>
+        #button
+            {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+button:hover {
+    opacity: 0.5;
+}
+#cancelbtn 
+            
+{
+    background-color: #e52c2c;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+    </style>
     </head>
-<body>
-    <h1>Sunshine Bakery</h1>
-    <form method="post" action="login.php">
-    <center><input type="text" name="admin_user" placeholder="Enter your username"><font color="red"><?php echo @$_GET['user']?></font><br>
-    <input type="password" name="admin_pass" placeholder="Enter your password"><font color="red"><?php echo @$_GET['pass']?></font><br><br>
-    <input type="submit" name="submit" value="sign in" onclick="bakery/index.php"></center>
+    <div style="clear:both"></div>
+    
+    <br>
+    
+    <div id="table_main">
+        <form name=myform method="POST" action="sign_final.php">
+<table align="center" cellspacing="15px" cellpadding="15px" bgcolor="rgba(255, 255, 255, 0.88)">
+    <tr><th><font color="white" size="5px">Username:</font></th><th><input type="text" name=username_box></th></tr>
+    <tr><th><font color="white" size="5px">Password:</font></th><th><input type="password" name="password_box"></th></tr>
+    <tr><th><input type="submit" name="login_btn" id="button"></th><th><input type="reset" value="reset" name="button" id="cancelbtn"></th></tr>
+    
+</table>
     </form>
-	<center><?php echo @$_GET['error'] ?></center>
-    </body>
+        </div>
 </html>
-
-<?php
-
-include('config.php');
-		if(isset($_POST['login'])){
-			$admin_name=$_SESSION['admin_name']=$_POST['admin_name'];
-			$admin_pass=$_POST['admin_pass'];
-			
-			$query="select * from login where user_name='$admin_name' AND user_password='$admin_pass'";
-			$run=mysql_query($query);
-			
-			if(mysql_num_rows($run)>0){
-				
-				echo "<script>window.open('view.php?logged=logged in successfully...!','_self')</script>";
-			}
-			else
-			{
-				echo "<script>alert('Password or User Name is incorrect!')</script>";
-			}
-		}
-?>
