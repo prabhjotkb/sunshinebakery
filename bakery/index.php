d53cc30add82e7f953273ce1e2f6fd2e530b55a0
prabhjotkb <41100291+prabhjotkb@users.noreply.github.com>
2018-07-30 04:45:12 +0000

    Update index.php

--- bakery/index.php
+++ bakery/index.php
@@ -101,6 +101,7 @@ $getResults= sqlsrv_query($conn, $tsql);
 				</tr>
 				<tr>
 					<td colspan="5" align="center"><input type="submit" value="Submit" name="submit"></td>
+					<td colspan="5" align="center"><input type="submit" value="Submit" name="home" onclick="index.html"></td>
 				</tr>
 			</table>
 			
