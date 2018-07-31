38aa75a83e896767cc1672b7ff90150d47552c7d
prabhjotkb <41100291+prabhjotkb@users.noreply.github.com>
2018-07-31 19:59:30 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+		$serverName = "sunbakery.database.windows.net";
+$connectionOptions = array(
+    "Database" => "Bakery",
+    "Uid" => "sunbakery",
+    "PWD" => "@bakery01"
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+				
+?>
