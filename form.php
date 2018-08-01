45ebca20985c8029aacbde6d7294b33e9c6de82b
prabhjotkb <41100291+prabhjotkb@users.noreply.github.com>
2018-08-01 16:03:19 +0530

    Update form.php

--- form.php
+++ form.php
@@ -55,6 +55,8 @@ $getResults= sqlsrv_query($conn, $tsql);
 <html>
 	<head>
 		<title>Bakery Form</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="style.css">
 	</head>
 	
